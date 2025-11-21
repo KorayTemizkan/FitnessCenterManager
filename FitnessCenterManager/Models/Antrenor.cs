27eb30eff0e4e78e4a86c33ce7f4ce2f8cd84aa1
Koray Temizkan <160160220+KorayTemizkan@users.noreply.github.com>
2025-11-21 23:27:53 +0300

    Antrenor ve Randevu modellerin yüklenmesi

---
+++ FitnessCenterManager/Models/Antrenor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FitnessCenterManager.Models
+{
+    public class Antrenor
+    {
+        public int AntrenorId { get; set; }
+
+        [Required, StringLength(60)]
+        public string AntrenorAd { get; set; } = string.Empty;
+
+        [Required, StringLength(60)]
+        public string AntrenorSoyad { get; set; } = string.Empty;
+
+        [Display(Name = "Uzmanlık Alanları"), StringLength(300)]
+        public string UzmanlikAlanlari { get; set; } = string.Empty; // Virgülle ayrılmış metin
+
+        public ICollection<AntrenorHizmet> AntrenorHizmetleri { get; set; } = new List<AntrenorHizmet>();
+        public ICollection<Randevu> Randevular { get; set; } = new List<Randevu>();
+    }
+}
