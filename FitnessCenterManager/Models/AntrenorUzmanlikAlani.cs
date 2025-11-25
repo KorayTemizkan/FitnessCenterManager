93107eb9f86a654d332f592f3c868a2d8f16bb42
Koray Temizkan <160160220+KorayTemizkan@users.noreply.github.com>
2025-11-25 23:54:16 +0300

    Antrenor hizmeti için ekler

---
+++ FitnessCenterManager/Models/AntrenorUzmanlikAlani.cs
@@ -0,0 +1,10 @@
+﻿namespace FitnessCenterManager.Models
+{
+    public class AntrenorUzmanlikAlani
+    {
+        public int AntrenorId { get; set; }
+        public Antrenor Antrenor { get; set; }
+        public int UzmanlikAlaniId { get; set; }
+        public UzmanlikAlani UzmanlikAlani { get; set; }
+    }
+}
